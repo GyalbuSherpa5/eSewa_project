f6112e47c9ee35499af9161f3d0fc6749a9451cc
Gyalbu Sherpa <gyalbusherpa555@gmail.com>
2023-03-27 11:42:05 +0545

    map

---
+++ src/week_2/day8/map/F1_IntroToMap.java
@@ -0,0 +1,25 @@
+package week_2.day8.map;
+
+public class F1_IntroToMap {
+    /*
+
+        Map is not the child interface of Collection
+        if we want to represent a group of individual objects as key value pairs then we should go for map.
+
+        both key are value are objects, duplicated keys are not allowed but values can be duplicate.
+
+     */
+
+    /*
+        SortedMap
+
+        It is the child interface of map
+        If we want to represent a group of key value pairs according to some sorting order of keys then we should go
+        for sortedmap.
+
+        NavigableMap
+        It is child interface of sorted map, it defines several utility methods for navigation purpose.
+
+        TreeMap is the child of NavigableMap.
+     */
+}
