ab00a11a24298a8c288d0464a800c54d5b01cb7d
Gyalbu Sherpa <gyalbusherpa555@gmail.com>
2023-03-30 10:18:55 +0545

    consumer

--- src/week_2/java1point8/F8_Predefined_Consumer.java
+++ src/week_2/java1point8/F8_Predefined_Consumer.java
@@ -2,6 +2,13 @@ package week_2.java1point8;
 
 import java.util.function.Consumer;
 
+class Movie{
+    String name;
+
+    public Movie(String name) {
+        this.name = name;
+    }
+}
 public class F8_Predefined_Consumer {
     /*
 
@@ -15,5 +22,13 @@ public class F8_Predefined_Consumer {
     public static void main(String[] args) {
         Consumer<String> c = i-> System.out.println(i);
         c.accept("don");
+
+        Consumer<Movie> c2 = i -> System.out.println(i.name + "ready ");
+        Consumer<Movie> c3 = i -> System.out.println(i.name + "ready but flop");
+        Consumer<Movie> c4 = i -> System.out.println(i.name + "ready not");
+        Consumer<Movie> cc = c2.andThen(c3).andThen(c4);
+
+        Movie m = new Movie("Spy");
+        cc.accept(m);
     }
 }
---
+++ src/week_2/java1point8/F9_Predefined_Supplier.java
@@ -0,0 +1,7 @@
+package week_2.java1point8;
+
+public class F9_Predefined_Supplier {
+    /*
+
+     */
+}
