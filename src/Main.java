ef581d01bfd436e5b42e384c2179c5fc7f6b06bb
Gyalbu Sherpa <gyalbusherpa555@gmail.com>
2023-03-30 15:40:21 +0545

    stream

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
-import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.LinkedList;
-
 class Main{
 
     public static void main(String[] args) {
-        LinkedList l = new LinkedList();
-        ArrayList a = new ArrayList();
-        String asdf = "f";
-
     }
 }
