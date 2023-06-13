a4989283786ae0582871eee7f676b371040bd2f8
GyalbuSherpa5 <gyalbusherpa555@gmail.com>
2023-06-13 13:58:39 +0545

    remove duplicate from sorted array

---
+++ src/leetCode/Solution03RemoveDuplicatesFromSortedArray26.java
@@ -0,0 +1,36 @@
+package leetCode;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+public class Solution03RemoveDuplicatesFromSortedArray26 {
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+
+        System.out.println(removeDuplicates(nums));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
+
+       // Using stream
+
+       /* int[] distinctNums = Arrays.stream(nums)
+                .distinct()
+                .toArray();
+
+        System.arraycopy(distinctNums, 0, nums, 0, distinctNums.length);
+
+        return distinctNums.length;*/
+    }
+}
