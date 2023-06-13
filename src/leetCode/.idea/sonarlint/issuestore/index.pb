28463b955f9a7bc276230c782e3f400422210244
GyalbuSherpa5 <gyalbusherpa555@gmail.com>
2023-06-13 10:28:12 +0545

    merge sorted array

---
+++ src/leetCode/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+\
+,src/leet_code/Solution02RemoveElement27.java,3/5/3548781c5b742099a2fcd983ab377a3131be7ee4
+_
+/src/leet_code/Solution01MergeSortedArray88.java,0/3/0343915ae94e6d08e78aecedec7e7899f615b69c
---
+++ src/leetCode/src/leet_code/Solution01MergeSortedArray88.java
@@ -0,0 +1,105 @@
+package leet_code;
+
+import java.util.Arrays;
+
+/*
+
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+Example 1:
+
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+Example 2:
+
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+Explanation: The arrays we are merging are [1] and [].
+The result of the merge is [1].
+
+Example 3:
+
+Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+Output: [1]
+Explanation: The arrays we are merging are [] and [1].
+The result of the merge is [1].
+Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+
+
+Constraints:
+
+    nums1.length == m + n
+    nums2.length == n
+    0 <= m, n <= 200
+    1 <= m + n <= 200
+    -109 <= nums1[i], nums2[j] <= 109
+
+
+Follow up: Can you come up with an algorithm that runs in O(m + n) time?
+*/
+
+public class Solution01MergeSortedArray88 {
+    public static void main(String[] args) {
+
+        int[] nums1 = {1, 2, 3, 5};
+        int[] num2 = {5, 3, 1};
+        int m = nums1.length;
+        int n = num2.length;
+
+        merge(nums1, num2, m, n);
+    }
+
+    public static void merge(int[] a, int[] b, int m, int n) {
+
+        int[] result = new int[m + n];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < m) {
+            result[k++] = a[i++];
+        }
+
+        while (j < n) {
+            result[k++] = b[j++];
+        }
+
+        Arrays.stream(result)
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+
+    // beta chat
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1; // Pointer for nums1
+        int j = n - 1; // Pointer for nums2
+        int k = m + n - 1; // Pointer for nums1 to fill the merged array
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        // If there are any remaining elements in nums2, copy them to nums1
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+
+}
