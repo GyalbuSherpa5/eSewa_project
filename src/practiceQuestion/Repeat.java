9b7811ba490a5585109fadbb0ed1e77e4dbd8a46
GyalbuSherpa5 <gyalbusherpa555@gmail.com>
2023-06-14 12:00:24 +0545

    map logic for repeated number in array

--- src/practiceQuestion/Repeat.java
+++ src/practiceQuestion/Repeat.java
@@ -1,14 +1,32 @@
 package practiceQuestion;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Repeat {
     public static void main(String[] args) {
         Repeat r = new Repeat();
-        int arr2[] = {1, 1, 2, 3, 5, 5, 4, 2, 3, 4};
+        int arr2[] = {1, 1, 2, 3, 5, 5, 4, 2, 3, 4, 34};
         int value = r.getSingNum(arr2);
         System.out.println("non repeated value is " + value);
 
     }
 
     public int getSingNum(int[] arr2) {
+
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int num : arr2) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry entry: map.entrySet()) {
+            int value = (int) entry.getValue();
+            if(value == 1){
+                System.out.println(entry.getKey());
+            }
+        }
+
         int temp = -1;
         for (int i = 0; i < arr2.length; i++) {
             Integer num = arr2[i];
