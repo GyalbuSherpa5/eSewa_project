7760a147512084194184314f198936347f7abb88
GyalbuSherpa5 <gyalbusherpa555@gmail.com>
2023-06-15 10:27:12 +0545

    reverse words in string

---
+++ src/leetCode/array/Solution09ReverseWordsInString.java
@@ -0,0 +1,40 @@
+package leetCode.array;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Solution09ReverseWordsInString {
+
+    public static void main(String[] args) {
+        String s = "a good   example   ";
+        System.out.println(reverseWords(s));
+    }
+
+    public static String reverseWords(String s) {
+
+        String[] words = s.split(" +");
+
+        Collections.reverse(Arrays.asList(words));
+
+        return String.join(" ", words).trim();
+
+        /*// Trim leading and trailing spaces
+        s = s.trim();
+
+        // Split the string into an array of words
+        String[] words = s.split("\\s+");
+
+        // Reverse the order of the words
+        StringBuilder reversed = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            reversed.append(words[i]);
+            if (i > 0) {
+                reversed.append(" ");
+            }
+        }
+
+        return reversed.toString();*/
+    }
+}
